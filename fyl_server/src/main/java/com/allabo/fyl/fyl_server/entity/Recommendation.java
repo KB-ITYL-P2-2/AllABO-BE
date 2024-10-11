6fa9f9198e0060fdfad594da9157350b0790c7d1
문준일 <answnsdlfwkd@naver.com>
2024-10-11 18:01:05 +0900

    feat: 중간 저장

--- fyl_server/src/main/java/com/allabo/fyl/fyl_server/entity/Recommendation.java
+++ fyl_server/src/main/java/com/allabo/fyl/fyl_server/entity/Recommendation.java
@@ -1,5 +1,9 @@
 package com.allabo.fyl.fyl_server.entity;
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 public class Recommendation {
     private Long id;
     private String name;             // 상품 이름 (카드 이름)
