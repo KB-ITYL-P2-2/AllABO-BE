ab7c813eba4ed673b1f041560c63489d1d024c39
Hannana <105469888+hancrysta1@users.noreply.github.com>
2024-10-15 03:23:18 +0900

    feat: 사용자 자산 포트폴리오 API (/assets/portfolio)

--- fyl_server/src/main/java/com/allabo/fyl/fyl_server/service/UserFinancialsServiceImpl.java
+++ fyl_server/src/main/java/com/allabo/fyl/fyl_server/service/UserFinancialsServiceImpl.java
@@ -2,6 +2,7 @@ package com.allabo.fyl.fyl_server.service;
 
 import com.allabo.fyl.fyl_server.dao.UserFinancialsRatioDAO;
 import com.allabo.fyl.fyl_server.dto.UserFinancialsDTO;
+import com.allabo.fyl.fyl_server.dto.UserPortfolioDTO;
 import com.allabo.fyl.fyl_server.repository.UserFinancialsRepository;
 import com.allabo.fyl.fyl_server.security.mapper.CustomerMapper;
 import lombok.RequiredArgsConstructor;
@@ -29,6 +30,15 @@ public class UserFinancialsServiceImpl implements UserFinancialsService{
     @Override
     public UserFinancialsDTO FindUserFinancials(String id) {
         UserFinancialsDTO dto = userFinancialsRepository.findUserFinancial(id);
+        if (dto == null || id == null){
+            throw new IllegalArgumentException("id가 null이거나 존재하는 financial 데이터가 없습니다.");
+        }
+        return dto;
+    }
+
+    @Override
+    public UserPortfolioDTO FindUserPortfolio(String id) {
+        UserPortfolioDTO dto = userFinancialsRepository.findUserPortfolio(id);
         if (dto == null || id == null){
             throw new IllegalArgumentException("id가 null이거나 존재하는 financial 데이터가 없습니다.");
         }
